5c5987d6e5927f936bab5bb49f4ca9fc7167995f
MobyDuck123 <152602889+MobyDuck123@users.noreply.github.com>
2023-12-15 20:50:11 +0800

    Challenge 3
    I fixed the dog game.
    > everything spawns correctly
    > I even did the bonus bits

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private bool canSpawn = true;
+    private float spawnDelay = 1.0f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            StartCoroutine(SpawnDelayTimer());
+        }
+    }
+
+    // Coroutine to control the spawn delay
+    IEnumerator SpawnDelayTimer()
+    {
+        canSpawn = false;
+        yield return new WaitForSeconds(spawnDelay);
+        canSpawn = true;
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    //private float startDelay = 1.0f;
+    
+    private float minSpawnInterval = 2.0f;
+    private float maxSpawnInterval = 5.0f;
+
+
+    void Start()
+    {
+        // Call the function to spawn a ball with a random delay initially
+        SpawnRandomBallWithRandomDelay();
+    }
+
+    // Spawn random ball at random x position at the top of the play area
+    void SpawnRandomBall()
+    {
+        // Generate random ball index and random spawn position
+        int randomBallIndex = Random.Range(0, ballPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // Instantiate random ball at random spawn location
+        Instantiate(ballPrefabs[randomBallIndex], spawnPos, ballPrefabs[randomBallIndex].transform.rotation);
+
+        // Schedule the next spawn with a new random interval
+        Invoke("SpawnRandomBallWithRandomDelay", Random.Range(minSpawnInterval, maxSpawnInterval));
+    }
+
+    // Function to spawn a ball with a random delay
+    void SpawnRandomBallWithRandomDelay()
+    {
+        // Spawn a ball
+        SpawnRandomBall();
+
+        // Schedule the next spawn with a new random interval
+        Invoke("SpawnRandomBallWithRandomDelay", Random.Range(minSpawnInterval, maxSpawnInterval));
+    }
+}
